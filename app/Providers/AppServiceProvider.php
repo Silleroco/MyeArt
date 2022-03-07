e2389428fc565ff42be5bde7ba80a6401406e6f3
xpestana <unibasileawe@gmail.com>
2022-03-07 14:25:02 -0430

    prueba de conexion y https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
+         \URL::forceScheme('https');
         Carbon::setLocale(config('app.locale'));
 
         /**
