2420b112edafcbbef5a904c4ac20bd1e0e1ae635
xpestana <unibasileawe@gmail.com>
2022-03-08 21:52:43 -0430

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,21 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         $this->call(CategorySeeder::class);
         $this->call(TagSeeder::class);
         $this->call(AttributeSeeder::class);
         $this->call(ElementSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
-        // $this->call(ProfileSeeder::class);
-        // $this->call(SellerSeeder::class);
-        // $this->call(GallerySeeder::class);
-        // $this->call(ArtistSeeder::class);
-        // $this->call(ArtworkSeeder::class);
-        // $this->call(ArtworkImageSeeder::class);
-        // $this->call(ArtworkElementSeeder::class);
-        // $this->call(LikeSeeder::class);
-        // $this->call(CommentSeeder::class);
+        $this->call(ProfileSeeder::class);
+        $this->call(SellerSeeder::class);
+        $this->call(GallerySeeder::class);
+        $this->call(ArtistSeeder::class);
+        $this->call(ArtworkSeeder::class);
+        $this->call(ArtworkImageSeeder::class);
+        $this->call(ArtworkElementSeeder::class);
+        $this->call(LikeSeeder::class);
+        $this->call(CommentSeeder::class);
     }
 }
