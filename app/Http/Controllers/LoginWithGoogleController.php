d4950bf7b4a4f4a3debc7f68f625e8fe9a951cf5
xpestana <unibasileawe@gmail.com>
2022-03-12 17:22:14 -0430

    redirect

--- app/Http/Controllers/LoginWithGoogleController.php
+++ app/Http/Controllers/LoginWithGoogleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Exception;
+use Redirect;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
